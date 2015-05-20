c7f9dd802dade164e8303308629b8f5926277890
ClemenceClem <clemencethune@hotmail.com>
2015-05-20 14:39:54 +0200

    Analyseur

--- VisuRDF/analyseur.cpp
+++ VisuRDF/analyseur.cpp
@@ -1,29 +1,44 @@
 #include "analyseur.h"
+#include <list>
+using namespace std;
 
-analyseur::analyseur()
-{
-    // attributs
-    // - une liste d'objets
-    // - une liste de types
+analyseur::analyseur() {
 
-    // get la liste d'objets
-    // en entree : on récupère la map de l'extracteur
+    // extractor :
+    // - une map avec tous les objets : (classe, liste de objets de la classe(map de propriétés de l'objet))
+    // - liste des classes (string)
+    // - liste des propriétés d'une classe (classe en entrée, liste de string en sortie)
+
+
+    // creer la liste d'objets
+    // on récupère la map de l'extracteur
     // pour chaque objet de la map, creer un objet Objet
-    // en sortie : renvoyer une liste de tous les objets
+    // l'ajouter au vecteur
 
-    // get la liste de classes
-    // en entree : on récupère la map de l'extracteur
+    // creer la liste de classes
+    // on récupère la map de l'extracteur
     // pour chaque nouveau type rencontré, on crée un type
-    // pour chaque type, on remplit la liste des propriétés
-    // en sortie : renvoyer une liste de tous les types
+    // pour chaque type, on remplit le vecteur des propriétés
 
     // compter le nombre de propriétés par type
-    // entree : nom de classe
-    // sortie : setNombreProprietes dans le Type
+    // pour chaque Type
+    // compter le nombre de proprietes
+    // setNombreProprietes dans le Type
 
     // compter le nombre d'objets par type
-    // entree : nom de classe
-    // sortie : setNombreObjets dans le Type
+    // pour chaque Type
+    // compter le nombre d'objets
+    // setNombreObjets dans le Type
 
 }
 
+analyseur::~analyseur() {
+}
+
+list<Objet> analyseur::getTousLesObjets() {
+    return tousLesObjets;
+}
+
+list<Type> analyseur::getTousLesTypes() {
+    return tousLesTypes;
+}
--- VisuRDF/analyseur.h
+++ VisuRDF/analyseur.h
@@ -1,14 +1,22 @@
 #ifndef ANALYSEUR_H
 #define ANALYSEUR_H
 
+#include <iostream>
+#include <list>
+#include <objet.h>
+#include <type.h>
 
 class analyseur
 {
-
 public:
     analyseur();
+    ~analyseur();
+    std::list<Objet> getTousLesObjets();
+    std::list<Type> getTousLesTypes();
 
-
+private:
+    std::list<Objet> tousLesObjets;
+    std::list<Type> tousLesTypes;
 };
 
 #endif // ANALYSEUR_H
