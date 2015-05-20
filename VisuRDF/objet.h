6242797cb41c9800fe761f52a876fd1890782a9e
Marjo57 <Marjo@macbook-air-de-marjory.home>
2015-05-20 20:43:11 +0200

    modif objet.h

--- VisuRDF/objet.h
+++ VisuRDF/objet.h
@@ -1,19 +1,29 @@
 #ifndef OBJET_H
 #define OBJET_H
 #include <QString>
-#include <QMap>
+#include <map>
+#include <list>
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef map<string,string> proprieteMap;
 
 class Objet
 {
 public:
     int id;
-    QString nomType;
-    QMap<QString, QString> proprietes;
+    string nomType;
+    proprieteMap proprietes;
     int largeurMax;
 
-    Objet(int id, QString nomType, QMap<QString,QString> proprietes);
+    Objet(int id, string nomType, proprieteMap proprietes);
     ~Objet();
-    void setLargeur(int l);
+
+    void setLargeur();
+    string getNomType();
+    proprieteMap getProprietes();
+    int getLargeur();
 
 
 };
