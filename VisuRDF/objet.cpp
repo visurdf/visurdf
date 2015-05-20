8fbfa59282c8a6677c6bb178bb16ed297b163276
Marjo57 <Marjo@macbook-air-de-marjory.home>
2015-05-20 21:30:50 +0200

    Merge branch 'marjo'
    Conflicts:
    	VisuRDF/objet.cpp
    	VisuRDF/objet.h

--- VisuRDF/objet.cpp
+++ VisuRDF/objet.cpp
@@ -1,6 +1,10 @@
 #include "objet.h"
 #include "visurdfextractor.h"
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> marjo
 Objet::Objet(int id, string nomType, ObjetRDF proprietes) : id(id), nomType(nomType), proprietes(proprietes)
 {
 
@@ -12,7 +16,45 @@ Objet::~Objet()
 
 }
 
-void Objet::setLargeur(int l){
-    largeurMax = l;
+int Objet::getLargeur(){
+    return largeurMax;
 }
 
+ObjetRDF Objet::getProprietes(){
+    return proprietes;
+}
+
+string Objet::getNomType(){
+    return nomType;
+}
+
+// Méthode qui va calculer la longueur max de la concaténation "nom propriété', "valeur propriété"
+void Objet::setLargeur(){
+
+    // On initialise largeur avec la longueur du string "nomType"
+    int largeur = nomType.size();
+
+    // On parcourt la map "proprietes" et on créé une liste de string en concaténant
+
+    list<string> proprieteConcatenee ;
+
+    for(ObjetRDF::const_iterator it = proprietes.begin(); it!=proprietes.end(); it++){
+        string nom = it->first;
+        list<string> listeValeur = it->second;
+        list<string>::iterator it2 = listeValeur.begin();
+        string valeur = *it2;
+        string concatene = nom + " : " + valeur;
+        proprieteConcatenee.push_back(concatene);
+    }
+
+    // On parcourt la liste "proprieteConcatenee" et met à jour éventuellement "largeur"
+    for(list<string>::iterator it = proprieteConcatenee.begin(); it!= proprieteConcatenee.end(); it++){
+        int calcul = (*it).size();
+        // On compare à largeur et on met éventuellement à jour
+        if(calcul>largeur){
+            largeur = calcul;
+        }
+    }
+
+    largeurMax = largeur;
+}
--- VisuRDF/objet.h
+++ VisuRDF/objet.h
@@ -1,16 +1,41 @@
 #ifndef OBJET_H
 #define OBJET_H
 #include <QString>
+<<<<<<< HEAD
 #include <QMap>
 #include "visurdfextractor.h"
+=======
+#include <map>
+#include <list>
+#include <iostream>
+#include <string>
+#include "visurdfextractor.h"
+
+using namespace std;
+
+typedef map<string,string> proprieteMap;
+>>>>>>> marjo
 
 class Objet
 {
 public:
+<<<<<<< HEAD
 
     Objet(int id, std::string nomType, ObjetRDF proprietes);
+=======
+    int id;
+    string nomType;
+    ObjetRDF proprietes;
+    int largeurMax;
+
+    Objet(int id, string nomType, ObjetRDF proprietes);
+>>>>>>> marjo
     ~Objet();
-    void setLargeur(int l);
+
+    void setLargeur();
+    string getNomType();
+    ObjetRDF getProprietes();
+    int getLargeur();
 
 private :
 
