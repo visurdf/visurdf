a8382a74f3d342294f4fa3b265147690dc7db7b6
Rodrigue TAPONNO <rodriguetaponno@gmail.com>
2015-05-26 20:02:38 +0200

    recuperation de la liste des objet d un type donne

--- VisuRDF/type.h
+++ VisuRDF/type.h
@@ -16,6 +16,25 @@ public:
     int getNbObjet();
     std::list<std::string> getProprietes();
     std::string getNom();
+    string toString(){
+
+           string tostring = "{Type: ";
+           string strprop = ", Props: [";
+           typedef list<string> ::const_iterator ListIterator;
+           int i =0;
+           for(ListIterator itr = proprietesNonVides.begin(); itr != proprietesNonVides.end(); itr++ ){
+               strprop.append(*itr);
+               if(i  != proprietesNonVides.size() -1)
+                   strprop.append(", ");
+               i++;
+           }
+           strprop.append("]");
+
+
+           return tostring
+                   .append(nomType)
+                   .append(strprop)
+                   .append("}");}
 
 private :
 
