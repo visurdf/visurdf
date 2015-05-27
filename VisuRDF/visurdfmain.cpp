36c67b5341408de671c12f501368b9e4871d4083
ClemenceClem <clemencethune@hotmail.com>
2015-05-27 16:47:26 +0200

    Style de codage

--- VisuRDF/visurdfmain.cpp
+++ VisuRDF/visurdfmain.cpp
@@ -13,59 +13,55 @@ int main(int argc, char *argv[])
     QApplication A(argc,argv);
 
     VisuRDFExtracteur visuRDFExtracteur;
-   // classesvg C;
+    // classesvg C;
 
     visuRDFExtracteur.parserTripletRdf(argv[1]);
     visuRDFExtracteur.afficherMap();
     visuRDFExtracteur.afficherRelations();
-   // C.drawSvg();
+    // C.drawSvg();
 
     cout << "\n Classes:\n" << endl;
-    set<string > listOfClass = visuRDFExtracteur.getClasses();
+    set<string > listeDesClasses = visuRDFExtracteur.getClasses();
 
-    for (set<string >::const_iterator grapheiter = listOfClass.begin(); grapheiter != listOfClass.end(); grapheiter++)       {
+    for (set<string >::const_iterator grapheIter = listeDesClasses.begin(); grapheIter != listeDesClasses.end(); grapheIter++)       {
         //set<string > listOfProperties = visuRDFExtractor.getProperties(*grapheiter);
-        cout << *grapheiter << "\t"<< endl;
+        cout << *grapheIter << "\t"<< endl;
         //for (set<string >::const_iterator props = listOfProperties.begin(); props != listOfProperties.end(); props++)
-          //  cout << *props << "\t";
+        //  cout << *props << "\t";
         //cout << endl<< endl;
     }
 
     VisuRDFAnalyseur* analyseur = new VisuRDFAnalyseur(&visuRDFExtracteur);
+    set<VisuRDFType*> tousLesTypes = analyseur->getTousLesTypes(false);
+    cout << "\n Nbre Types : " << tousLesTypes.size() << endl;
 
-
-     set<VisuRDFType*> allTypes = analyseur->getTousLesTypes(false);
-     cout << "\n Nbre Types : " << allTypes.size() << endl;
-     typedef set<VisuRDFType* > ::const_iterator ListIterator;
-    for(ListIterator itr = allTypes.begin(); itr != allTypes.end(); itr++ )
+    typedef set<VisuRDFType*> ::const_iterator ListIterator;
+    for(ListIterator itr = tousLesTypes.begin(); itr != tousLesTypes.end(); itr++ )
         cout << (*itr)->toString() << endl;
 
-    allTypes = analyseur->getTousLesTypes(true);
-         cout << "\n Nbre Types avec proprietes significatives: " << allTypes.size() << endl;
-         typedef set<VisuRDFType* > ::const_iterator ListIterator;
-        for(ListIterator itr = allTypes.begin(); itr != allTypes.end(); itr++ )
-            cout << (*itr)->toString() << endl;
+    tousLesTypes = analyseur->getTousLesTypes(true);
+    cout << "\n Nbre Types avec proprietes significatives: " << tousLesTypes.size() << endl;
+    typedef set<VisuRDFType*> ::const_iterator ListIterator;
+    for(ListIterator itr = tousLesTypes.begin(); itr != tousLesTypes.end(); itr++ )
+        cout << (*itr)->toString() << endl;
 
-
-    VisuRDFType* unType = analyseur->getTypeParNom(*listOfClass.begin(), false);
-
+    VisuRDFType* unType = analyseur->getTypeParNom(*listeDesClasses.begin(), false);
     cout << endl<< endl << unType->toString() << endl;
 
-    unType = analyseur->getTypeParNom(*listOfClass.begin(), true);
-
+    unType = analyseur->getTypeParNom(*listeDesClasses.begin(), true);
     cout << endl<< endl << unType->toString() << endl;
 
-   set<VisuRDFObjet*>listOfObject = analyseur->getObjetsParType(*listOfClass.begin(), true);
-   typedef set<VisuRDFObjet* > ::const_iterator ObjectIterator;
-  for(ObjectIterator itr = listOfObject.begin(); itr != listOfObject.end(); itr++ )
-    cout << (*itr)->toString() << endl;
+    set<VisuRDFObjet*>listeDesObjets = analyseur->getObjetsParType(*listeDesClasses.begin(), true);
+    typedef set<VisuRDFObjet* > ::const_iterator ObjectIterator;
+    for(ObjectIterator itr = listeDesObjets.begin(); itr != listeDesObjets.end(); itr++ )
+        cout << (*itr)->toString() << endl;
 
     VisuRDFDessinateur* dessinateur = new VisuRDFDessinateur(analyseur);
     VisuRDFGenerateur* generateur = new VisuRDFGenerateur(dessinateur);
     //generateur->dessinTableau(unType, 20, 20);
     generateur->dessin();
-   // dessinateur->dessinTableau(unType, 20, 20);
-  //  cout << "largeur tableau : " << dessinateur->calculLargeurColonne(unType, "Equipement.port") << endl;
+    // dessinateur->dessinTableau(unType, 20, 20);
+    //  cout << "largeur tableau : " << dessinateur->calculLargeurColonne(unType, "Equipement.port") << endl;
 
     return 0;
 }
