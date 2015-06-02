f2ac2cb68ebad504e2a2640bb32e78f943d3a703
ClemenceClem <clemencethune@hotmail.com>
2015-06-02 09:41:05 +0200

    Encodage texte UTF-8

--- VisuRDF/visurdfmain.cpp
+++ VisuRDF/visurdfmain.cpp
@@ -10,6 +10,10 @@
 int main(int argc, char *argv[])
 {
 
+    // Encodage UTF-8
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
     QApplication A(argc,argv);
 
     VisuRDFExtracteur visuRDFExtracteur;
